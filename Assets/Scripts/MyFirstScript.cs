9d1cc577ff371ff933fefc5b1d0b3661208df266
feerraan <fazpiazuadrover@gmail.com>
2023-10-03 12:43:27 +0200

    primer dia scripting

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{ /*
+     Comentario linea 1
+     Comentario linea 2
+     Comentario linea 3 
+    */
+
+    // Variable que representa la edad de un jugador
+    private int age = 18; // = 0;
+    // Variable que representa la velocidad del jugador
+    private float speed = 1.5f; // = 0.0f
+    //Variable que representa si el jugador está muerto o no
+    private bool isGameOver = false; // = false
+    //Variable que representa el nombre del jugador
+    private string username = "Pepin"; // = ""
+}
